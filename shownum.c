0b85133e28d34bf0576f60a4f5769e89b68b3b50
pomac <pomac@61bdd00a-bcee-11db-b5ab-9d6d16b2e755>
2008-02-22 21:01:42 +0000

    Documented the code some more.
    git-svn-id: svn://pomac.netswarm.net/shownum@18 61bdd00a-bcee-11db-b5ab-9d6d16b2e755

--- shownum.c
+++ shownum.c
@@ -68,8 +68,8 @@ typedef   signed __int64 int64_t;
 
 typedef enum
 {
-	ENDIAN_BIG,
-	ENDIAN_LITTLE
+	ENDIAN_BIG,		/**< for big endian, default */
+	ENDIAN_LITTLE		/**< for little endian */
 } endian_t;
 
 struct _parse_data_t;
@@ -78,11 +78,11 @@ typedef int (*parse_func_t)(unsigned char p, struct _parse_data_t *data);
 
 typedef struct _parse_data_t
 {
-	parse_func_t parse;
-	uint64_t value;
-	int64_t offset; /* Note: Assumed to be int on several places below */
-	int is_negative;
-	char name[15];
+	parse_func_t parse;		/**< a pointer to a parser function */
+	uint64_t value;		/**< where we save the parsed values */
+	int64_t offset; 		/**< here we store any kind of additional state we need while parsing */
+	int is_negative;		/**< here we store if we have read a negative value or not */
+	char name[15];			/**< the name of the type of value we are parsing */
 } parse_data_t;
 
 static int parse_dec (unsigned char p, parse_data_t *data);
@@ -100,6 +100,11 @@ static int parse_bin (unsigned char p, parse_data_t *data);
  * Parse functions
  * ========================================================================= */
 
+/** Parse a decimal string to a integer value.
+ * @param p a character from the end of the input string.
+ * @param data the data struct for the parser.
+ * @return 1 if we can handle the calue else 0.
+ */
 static int
 parse_dec (unsigned char p, parse_data_t *data)
 {
@@ -125,6 +130,11 @@ parse_dec (unsigned char p, parse_data_t *data)
 		return 0;
 }
 
+/** Parse a hexadecimal string to a integer value.
+ * @param p a character from the end of the input string.
+ * @param data the data struct for the parser.
+ * @return 1 if we can handle the value else 0.
+ */
 static int
 parse_hex (unsigned char p, parse_data_t *data)
 {
@@ -158,6 +168,11 @@ parse_hex (unsigned char p, parse_data_t *data)
 		return 0;
 }
 
+/** Parse a character string to a integer value.
+ * @param p a character from the end of the input string.
+ * @param data the data struct for the parser.
+ * @return 1 if we can handle the value else 0.
+ */
 static int
 parse_chr (unsigned char p, parse_data_t *data)
 {
@@ -178,6 +193,11 @@ parse_chr (unsigned char p, parse_data_t *data)
 	return 1;
 }
 
+/** Parse a binary string (1 or 0) to a integer value.
+ * @param p a character from the end of the input string.
+ * @param data the data struct for the parser.
+ * @return 1 if we can handle the value else 0.
+ */
 static int
 parse_bin (unsigned char p, parse_data_t *data)
 {
@@ -202,85 +222,10 @@ parse_bin (unsigned char p, parse_data_t *data)
 }
 
 /* =========================================================================
- * Helper functions
- * ========================================================================= */
-
-static unsigned int
-get_no_bytes(const parse_data_t * const data)
-{
-	uint64_t value = data->is_negative ? data->value * -1 : data->value;
-	unsigned int no_bytes = 1, high = 0;
-
-	/* 64 bit or 32 bit? */
-	if (GET_HI(value))
-	{
-		value = GET_HI(value);
-		high = 1;
-	}
-
-	/* Binary search for the highest byte with a value */
-	if (value & 0xffff0000)
-	{
-		if ((value & 0xff000000) == 0)
-			no_bytes = 3;
-		else
-			no_bytes = 4;
-	}
-	else if (value & 0x0000ff00)
-		no_bytes = 2;
-
-	if (high)
-		no_bytes += 4;
-	
-	return no_bytes;
-}
-
-static unsigned int
-switch_endian(parse_data_t *data, endian_t endian)
-{
-	unsigned int no_bytes = get_no_bytes(data);
-
-	switch (endian)
-	{
-		case ENDIAN_LITTLE:
-		{
-			unsigned char *bytes = (unsigned char *)&data->value;
-			uint64_t value = 0;
-			int offset = -16; /* Initial bit shift should be zero, tus we have to set a negative value here */
-
-			/* We need a even value */
-			if (no_bytes % 2 != 0)
-				no_bytes++;
-
-			/* Since we have a even value we can process two bytes at a time. */
-			switch (no_bytes)
-			{
-				case 8:
-					value |= (((uint64_t)bytes[7] | ((uint64_t)bytes[6] << 8)) << (unsigned int)(offset += 16));
-					/*@fallthrough@*/
-				case 6:
-					value |= (((uint64_t)bytes[5] | ((uint64_t)bytes[4] << 8)) << (unsigned int)(offset += 16));
-					/*@fallthrough@*/
-				case 4:
-					value |= (((uint64_t)bytes[3] | ((uint64_t)bytes[2] << 8)) << (unsigned int)(offset += 16));
-					/*@fallthrough@*/
-				case 2:
-					value |= (((uint64_t)bytes[1] | ((uint64_t)bytes[0] << 8)) << (unsigned int)(offset += 16));
-			}
-			data->value = value;
-			break;
-		}
-		default:
-			break;
-	}
-	return no_bytes;
-}
-
-/* =========================================================================
  * Help text
  * ========================================================================= */
 
-static const char const usage_information[] = {
+static const char usage_information[] = {
 	"shownum " VERSION " - http://zapek.com/?page_id=17\n"
 	"            | http://pomac.netswarm.net/misc/\n\n"
 	"usage: shownum <val>\n"
@@ -296,67 +241,6 @@ static const char const usage_information[] = {
 	" -le -- little endian\n"
 	" -be -- big endian (default)\n"
 };
-
-/* =========================================================================
- * Output functions
- * ========================================================================= */
-
-static void
-print_ascii(const parse_data_t * const data, const unsigned int no_bytes)
-{
-	char *tmp = NULL, buffer[65] = {'\0'};
-	int idx = 0;
-
-	for (tmp = buffer, idx = (int)((no_bytes -1) << 3); idx > -1; idx -= 8, tmp++)
-	{
-		if (!isprint(*tmp = (char)(((((uint64_t)0xff << (unsigned int)idx)) & data->value) >> (unsigned int)idx)))
-			*tmp = '.';
-		if (idx == 32)
-		{
-			*(++tmp) = ' ';
-			*(++tmp) = '|';
-			*(++tmp) = ' ';
-		}
-	}
-	printf ("Asc: %s\n", buffer);
-}
-
-static void
-print_binary(const parse_data_t * const data, const unsigned int no_bytes)
-{
-	char *tmp = NULL, buffer[70] = {'\0'};
-	unsigned int idx = no_bytes << 3;
-	uint64_t bit = (uint64_t)1 << (unsigned int)(idx - 1);
-
-	for (tmp = buffer, idx = 64 - idx; idx < 64; idx++)
-	{
-		*tmp++ = (data->value & bit) ? '1' : '0';
-		bit >>= 1;
-
-		if (idx == 31)
-		{
-			*tmp++ = ' ';
-			*tmp++ = '|';
-			*tmp++ = ' ';
-		}
-	}
-	printf ("Bin: %s\n", buffer);
-}
-
-static void
-print_ruler(const unsigned int no_bytes)
-{
-	int nibble = 0;
-
-	printf ("     ");
-	for (nibble = (int)no_bytes; nibble; nibble--)
-	{
-		printf ("^---+---");
-		if (nibble == 5)
-			printf ("   ");
-	}
-	printf ("\n\n");
-}
 
 #ifdef TEST_PARSERS
 
@@ -456,6 +340,185 @@ main (void)
 
 #else /* TEST_PARSERS */
 
+/* =========================================================================
+ * Helper functions
+ * ========================================================================= */
+
+/** Get the number of bytes the value occupies.
+ *
+ * This is used so we can minimize the size of the value we output.
+ *
+ * @param data the parser data.
+ * @return the number of bytes this value occupies.
+ */
+static unsigned int
+get_no_bytes(const parse_data_t * const data)
+{
+	uint64_t value = data->is_negative ? data->value * -1 : data->value;
+	unsigned int no_bytes = 1, high = 0;
+
+	/* 64 bit or 32 bit? */
+	if (GET_HI(value))
+	{
+		value = GET_HI(value);
+		high = 1;
+	}
+
+	/* Binary search for the highest byte with a value */
+	if (value & 0xffff0000)
+	{
+		if ((value & 0xff000000) == 0)
+			no_bytes = 3;
+		else
+			no_bytes = 4;
+	}
+	else if (value & 0x0000ff00)
+		no_bytes = 2;
+
+	if (high)
+		no_bytes += 4;
+	
+	return no_bytes;
+}
+
+/** Switch the endian of a value.
+ *
+ * This function calls get_no_bytes and sometimes has to add a byte for the 
+ * endian switcheroo, thus it returns the number of bytes the value occupies.
+ *
+ * It modifies the value in the data struct.
+ *
+ * @param data the parser data.
+ * @param endian what endian it should be in.
+ * @return the number of bytes this value occupies.
+ */
+static unsigned int
+switch_endian(parse_data_t *data, endian_t endian)
+{
+	unsigned int no_bytes = get_no_bytes(data);
+
+	switch (endian)
+	{
+		case ENDIAN_LITTLE:
+		{
+			unsigned char *bytes = (unsigned char *)&data->value;
+			uint64_t value = 0;
+			int offset = -16; /* Initial bit shift should be zero, tus we have to set a negative value here */
+
+			/* We need a even value */
+			if (no_bytes % 2 != 0)
+				no_bytes++;
+
+			/* Since we have a even value we can process two bytes at a time. */
+			switch (no_bytes)
+			{
+				case 8:
+					value |= (((uint64_t)bytes[7] | ((uint64_t)bytes[6] << 8)) << (unsigned int)(offset += 16));
+					/*@fallthrough@*/
+				case 6:
+					value |= (((uint64_t)bytes[5] | ((uint64_t)bytes[4] << 8)) << (unsigned int)(offset += 16));
+					/*@fallthrough@*/
+				case 4:
+					value |= (((uint64_t)bytes[3] | ((uint64_t)bytes[2] << 8)) << (unsigned int)(offset += 16));
+					/*@fallthrough@*/
+				case 2:
+					value |= (((uint64_t)bytes[1] | ((uint64_t)bytes[0] << 8)) << (unsigned int)(offset += 16));
+			}
+			data->value = value;
+			break;
+		}
+		default:
+			break;
+	}
+	return no_bytes;
+}
+
+/* =========================================================================
+ * Output functions
+ * ========================================================================= */
+
+/** Print the ascii values if any.
+ *
+ * Prints all "printable" characters in a value.
+ *
+ * @param data the parse data.
+ * @param no_bytes the number of bytes we should output.
+ */
+static void
+print_ascii(const parse_data_t * const data, const unsigned int no_bytes)
+{
+	char *tmp = NULL, buffer[65] = {'\0'};
+	int idx = 0;
+
+	for (tmp = buffer, idx = (int)((no_bytes -1) << 3); idx > -1; idx -= 8, tmp++)
+	{
+		if (!isprint(*tmp = (char)(((((uint64_t)0xff << (unsigned int)idx)) & data->value) >> (unsigned int)idx)))
+			*tmp = '.';
+		if (idx == 32)
+		{
+			*(++tmp) = ' ';
+			*(++tmp) = '|';
+			*(++tmp) = ' ';
+		}
+	}
+	printf ("Asc: %s\n", buffer);
+}
+
+/** Print the value in a binary representation.
+ *
+ * Basically prints the value as ones and zeros.
+ *
+ * @param data the parse data.
+ * @param no_bytes the number of bytes we should output.
+ */
+static void
+print_binary(const parse_data_t * const data, const unsigned int no_bytes)
+{
+	char *tmp = NULL, buffer[70] = {'\0'};
+	unsigned int idx = no_bytes << 3;
+	uint64_t bit = (uint64_t)1 << (unsigned int)(idx - 1);
+
+	for (tmp = buffer, idx = 64 - idx; idx < 64; idx++)
+	{
+		*tmp++ = (data->value & bit) ? '1' : '0';
+		bit >>= 1;
+
+		if (idx == 31)
+		{
+			*tmp++ = ' ';
+			*tmp++ = '|';
+			*tmp++ = ' ';
+		}
+	}
+	printf ("Bin: %s\n", buffer);
+}
+
+/** Print the ruler.
+ *
+ * The ruler shows the nibble and the byte borders of the binary representation.
+ * '^' is a byte separator while '+' is a nibble separator.
+ *
+ * @param no_bytes the number of bytes we should show.
+ */
+static void
+print_ruler(const unsigned int no_bytes)
+{
+	int nibble = 0;
+
+	printf ("     ");
+	for (nibble = (int)no_bytes; nibble; nibble--)
+	{
+		printf ("^---+---");
+		if (nibble == 5)
+			printf ("   ");
+	}
+	printf ("\n\n");
+}
+
+/* =========================================================================
+ * Where it all comes together.
+ * ========================================================================= */
+
 int
 main (int argc, char **argv)
 {
@@ -463,10 +526,10 @@ main (int argc, char **argv)
 	char *last = NULL, *first = NULL;
 
 	parse_data_t data[] = {
-		{parse_dec, 0, 1, 0, "Decimal"},	/* The offset value has to be 1 here or all mults will fail! */
-		{parse_hex, 0, 0, 0, "Hexadecimal"},
-		{parse_bin, 0, 0, 0, "Binary"},
-		{parse_chr, 0, 0, 0, "Character"},	/* Wants the negation sign as a actual character */
+		{parse_dec, 0, 1, 0, "Decimal"},		/* set up the decimal parser, it needs a start offset of 1 */
+		{parse_hex, 0, 0, 0, "Hexadecimal"},	/* set up the hexadecimal parser */
+		{parse_bin, 0, 0, 0, "Binary"},		/* set up the binary parser      */
+		{parse_chr, 0, 0, 0, "Character"},		/* set up the character parser   */
 	};
 
 	/* No options, no input data */
